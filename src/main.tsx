0101a31b491b0bea60947f2321d407e432116880
Leslie-57blocks <leslie@57blocks.com>
2024-08-06 14:28:09 +0800

    feat: init eruda
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    CC:
    
    Maniphest Tasks:

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import WebApp from '@twa-dev/sdk';
+import eruda from 'eruda';
 
 import App from './App.tsx';
 
 import './index.css';
 
+eruda.init();
 WebApp.ready();
 WebApp.enableClosingConfirmation();
 
